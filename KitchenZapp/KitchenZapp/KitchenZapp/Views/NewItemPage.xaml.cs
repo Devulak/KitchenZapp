f5e816345e85eabf84283150aafa015ad0e1af06
Devulak <Devulak@users.noreply.github.com>
2020-05-07 04:23:27 +0200

    Add design

--- KitchenZapp/KitchenZapp/KitchenZapp/Views/NewItemPage.xaml.cs
+++ KitchenZapp/KitchenZapp/KitchenZapp/Views/NewItemPage.xaml.cs
@@ -10,7 +10,7 @@ namespace KitchenZapp.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewItemPage : ContentPage
     {
         public Account Item { get; set; }
